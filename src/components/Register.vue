ce00873caa2b5be036bacb00a9edacfcd6d57843
Marvin Marqua <marvinmarqua@gmail.com>
2023-01-21 20:14:53 +0100

    deleted boilerplate code and started register screen

---
+++ src/components/Register.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="register-container" style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+    <form id="register-form">
+        <label for="username">Emailaddress:</label>
+        <input type="text" id="username" name="username">
+                <button @click="register">Register</button>
+                </form>
+
+            </div>
+</template>
+<script>
+export default {
+    methods: {
+        register(){
+            
+        }
+    },
+}
+</script>
