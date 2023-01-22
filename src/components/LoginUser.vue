ed41ef69ab72092f3ed2fa78007303d679d4fe31
Marvin Marqua <marvinmarqua@gmail.com>
2023-01-22 22:09:46 +0100

    linted

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="input-group mb-3 w-50">
-    <input 
-      type="text" 
-      class="form-control" 
-      placeholder="Add your Emailaddresse" 
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Add your Emailaddresse"
       aria-label="Emailaddress"
       aria-describedby="btn-login"
-      v-model="useremail" />
-    <button 
-      @click="login" 
-      class="btn btn-outline-success" 
-      type="button" 
-      id="btn-login">
+      v-model="useremail"
+    />
+    <button
+      @click="login"
+      class="btn btn-outline-success"
+      type="button"
+      id="btn-login"
+    >
       Login
     </button>
   </div>
@@ -22,20 +24,20 @@ export default {
   data() {
     return {
       useremail: "",
-      statusLoginText: ""
+      statusLoginText: "",
     };
   },
   methods: {
     async login() {
       var p = new Passwordless.Client({
-        apiKey: "mmrqa:public:0b1456c994ef46079ab72c1ed5c1a08b"
+        apiKey: "mmrqa:public:0b1456c994ef46079ab72c1ed5c1a08b",
       });
 
       var alias = this.useremail;
 
       // yUf6_wWdDh02ItIvnCKT_02ItIvn...
       var token = await p.signinWithAlias(alias);
-     
+
       const param = { SignInToken: token };
       const signInUserResult = await Parse.Cloud.run("signinUser", param);
       const verifiedUser = JSON.parse(signInUserResult);
@@ -43,9 +45,8 @@ export default {
       console.log(verifiedUser.success);
 
       if (verifiedUser.success === true) {
-        this.statusLoginText = "You are In!"
+        this.statusLoginText = "You are In!";
       }
-
     },
   },
 };
